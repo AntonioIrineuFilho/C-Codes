d28274e3f14f78e7191e7e667797c29eb08c691e
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-11-12 20:20:10 +0000

    update

--- lista01/2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char nome[100];
-    printf("Digite seu nome: ");
-    scanf("%s", &nome);
-    printf("Olá %s, bem-vindo(a) ao mundo da linguagem de programação C! ", nome);
-    return 0;
-}
